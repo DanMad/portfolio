2c0fcece2d46a90808022067a1928961a2c5936e
Daniel Maddison <dmaddison@icloud.com>
2021-07-04 14:03:22 +1000

    Added Heading Component

---
+++ src/components/heading/index.jsx
@@ -0,0 +1,21 @@
+import { BEM } from '../../utils';
+import './styles.scss';
+
+const bem = BEM('h');
+const { getBlock } = bem;
+
+const Heading = ({ children, navigable, style, type = 1 }) => {
+  const Tag = `h${type}`;
+
+  return (
+    <Tag
+      className={`${getBlock()}${type}`}
+      id={navigable && children.toKebabCase()}
+      style={style}
+    >
+      {children.toTitleCase()}
+    </Tag>
+  );
+};
+
+export { Heading as default };
