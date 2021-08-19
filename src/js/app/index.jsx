7e40a3450fcae86e6aafb54990434f8d65a50aad
Daniel Maddison <dmaddison@icloud.com>
2021-08-19 21:28:23 +1000

    Application updates

--- src/js/app/index.jsx
+++ src/js/app/index.jsx
@@ -3,69 +3,89 @@ import React, { useEffect, useState } from 'react';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import Footer from '../layouts/footer';
 import Nav from '../layouts/nav';
-import Page from '../layouts/page';
+// import Page from '../layouts/page';
 import Context from '../components/context';
+import Preloader from '../components/preloader';
 import '../../scss/styles.scss';
 
 const App = () => {
-  const [state, setState] = useState({
-    loading: true,
-    payload: {},
-  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [isRouting, setIsRouting] = useState(false);
+  const [data, setdata] = useState();
+  const [preloaderClassName, setPreloaderClassName] = useState('');
 
   useEffect(() => {
-    fetch('/api/data.json', {
-      headers: {
-        'Content-Type': 'application/json',
-        Accept: 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((payload) => setState({ loading: false, payload }))
-      .catch((error) => console.error(error));
-  }, []);
+    if (isLoading) {
+      setPreloaderClassName('mounted');
 
-  if (state.loading) {
-    return <span>Loading...</span>;
-  } else {
-    return (
-      <Context.Provider
-        value={{
-          appState: state,
-          setAppState: setState,
-        }}
-      >
+      let timer;
+
+      fetch('/api/data.json', {
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      })
+        .then((response) => {
+          // Mock a response time
+          return new Promise((resolve) => {
+            timer = setTimeout(() => {
+              resolve(response.json());
+            }, 5000);
+          });
+        })
+        .then((json) => {
+          setdata(json);
+          setPreloaderClassName('');
+        })
+        .catch((error) => console.error(error));
+
+      return () => {
+        clearTimeout(timer);
+      };
+    }
+  }, [isLoading]);
+
+  return (
+    <Context.Provider
+      value={{
+        isRouting,
+        preloaderClassName,
+        setIsLoading,
+        setIsRouting,
+      }}
+    >
+      {isLoading ? (
+        <Preloader />
+      ) : (
         <BrowserRouter>
-          <Nav pages={state.payload.pages} />
-          <Switch>
-            <Redirect exact from="/" to="/portfolio" />
-            {state.payload.pages.map((page) => (
-              <Route
-                exact
-                key={page.title}
-                path={`/${page.title.toKebabCase()}`}
-              >
-                {page.title === 'portfolio' ? (
-                  <Page
-                    innerHTML={page.innerHTML}
-                    slides={state.payload.projects}
-                    title={page.title}
-                    type={'slideshow'}
-                  />
-                ) : (
-                  <Page innerHTML={page.innerHTML} title={page.title} />
-                )}
-              </Route>
-            ))}
-          </Switch>
-          <Footer
-            author={state.payload.author}
-            created={state.payload.created}
-          />
+          <Nav pages={data.pages} />
+          {/* <Switch>
+              <Redirect exact from="/" to="/portfolio" />
+              {data.pages.map((page) => (
+                <Route
+                  exact
+                  key={page.title}
+                  path={`/${page.title.toKebabCase()}`}
+                >
+                  {page.title === 'portfolio' ? (
+                    <Page
+                      innerHTML={page.innerHTML}
+                      slides={data.projects}
+                      title={page.title}
+                      type={'slideshow'}
+                    />
+                  ) : (
+                    <Page innerHTML={page.innerHTML} title={page.title} />
+                  )}
+                </Route>
+              ))}
+            </Switch> */}
+          <Footer author={data.author} created={data.created} />
         </BrowserRouter>
-      </Context.Provider>
-    );
-  }
+      )}
+    </Context.Provider>
+  );
 };
 
 export { App as default };
--- src/js/components/preloader/index.jsx
+++ src/js/components/preloader/index.jsx
@@ -1,10 +1,25 @@
+import { useContext } from 'react';
+import Context from '../context';
 import { BEM } from '../../utils';
 
 const { getBlock, getElement } = BEM('preloader');
 
 const Preloader = () => {
+  const { preloaderClassName, setIsLoading } = useContext(Context);
+
+  const handleTransitionEnd = () => {
+    if (preloaderClassName === '') {
+      setIsLoading(false);
+    }
+  };
+
+  const classNames =
+    preloaderClassName === ''
+      ? getBlock()
+      : `${getBlock()} ${preloaderClassName}`;
+
   return (
-    <div className={getBlock()}>
+    <div className={classNames} onTransitionEnd={handleTransitionEnd}>
       <div className={getElement('track')}>
         <div className={getElement('bar')} />
       </div>
