7e72068a277ff5e0cdb5199e3902fcf4b8480631
Daniel Maddison <dmaddison@icloud.com>
2021-08-19 21:28:53 +1000

    Configuration updates

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,13 @@
+const name = {
+  first: 'daniel',
+  last: 'maddison',
+};
+
+name.initials = name.first.charAt(0) + name.last.charAt(0);
+
+const namespace = name.initials;
 const created = '2021-08-06';
 const domain = 'www.danielmaddison.io';
-const namespace = 'dm';
 const pages = ['', 'about', 'contact', 'portfolio'];
 const protocol = 'https://';
 
