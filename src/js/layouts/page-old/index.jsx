622b04867fc30003f839b19579659718b348c5e4
Daniel Maddison <dmaddison@icloud.com>
2021-08-21 20:05:25 +1000

    Refactored component

--- src/js/layouts/page-old/index.jsx
+++ src/js/layouts/page-old/index.jsx
@@ -6,29 +6,6 @@ import Preloader from '../../components/preloader';
 import Slide from '../../components/slide';
 
 const Page = (props) => {
-  // export default function App() {
-  // const [animateBeforeUnmount, setAnimateBeforeUnmount] = React.useState(false);
-  // const [isRendered, setIsRendered] = React.useState(true);
-  // const divRef = React.useRef();
-
-  // const handleAnimationEnd = () => {
-  //   setIsRendered(false);
-  //   setAnimateBeforeUnmount(false);
-  // };
-
-  // return (
-  //   <>
-  //     <button onClick={() => setAnimateBeforeUnmount(true)}>Toggle</button>
-  //     {isRendered && (
-  //       <div
-  //         onAnimationEnd={handleAnimationEnd}
-  //         ref={divRef}
-  //         className={`bg-square ${animateBeforeUnmount && "fade-out"}`}
-  //       />
-  //     )}
-  //   </>
-  // );
-
   // useEffect(() => {
   //   setTimeout(() => setState({ loading: false }), 3000);
   // }, []);
