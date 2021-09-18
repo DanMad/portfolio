ebdc2ecfdf62356e7f03141ae653633d4c1baca5
Daniel Maddison <dmaddison@icloud.com>
2021-09-18 20:08:16 +1000

    Added toPageConfig()

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -3,6 +3,7 @@ import hasDisplayP3Support from './has-display-p3-support';
 import setTheme from './set-theme';
 import toCSSVariable from './to-css-variable';
 import toDisplayP3 from './to-display-p3';
+import toPageConfig from './to-page-config';
 import toRange from './to-range';
 import toStrokeStyles from './to-stroke-styles';
 import useEventListener from './use-event-listener';
@@ -13,6 +14,7 @@ const utils = {
   setTheme,
   toCSSVariable,
   toDisplayP3,
+  toPageConfig,
   toRange,
   toStrokeStyles,
   useEventListener,
@@ -24,6 +26,7 @@ export {
   setTheme,
   toCSSVariable,
   toDisplayP3,
+  toPageConfig,
   toRange,
   toStrokeStyles,
   useEventListener,
---
+++ src/js/utils/to-page-config.js
@@ -0,0 +1,49 @@
+import 'on-the-case';
+import defaultTheme from '../../config/theme';
+import toCSSVariable from './to-css-variable';
+// import toDisplayP3 from './to-display-p3';
+
+const toPageConfig = (name, options = {}) => {
+  const pageConfig = {
+    fileName: 'index.html',
+    path: '',
+    title: name.toTitleCase().trim(),
+    styles: '<style>:root{',
+  };
+
+  if (options.renameFile !== undefined && options.renameFile) {
+    pageConfig.fileName = name.toKebabCase().trim() + '.html';
+  }
+
+  if (options.parentRoute !== undefined) {
+    pageConfig.path += options.parentRoute.toKebabCase().trim() + '/';
+  }
+
+  if (options.route === undefined || options.route) {
+    pageConfig.path += name.toKebabCase().trim() + '/';
+  }
+
+  let theme = defaultTheme;
+
+  if (options.theme !== undefined) {
+    theme = { ...defaultTheme, ...options.theme };
+  }
+
+  Object.keys(theme).map((themeVar) => {
+    pageConfig.styles += toCSSVariable(themeVar) + `:${theme[themeVar]};`;
+  });
+
+  // pageConfig.styles += '}@supports(color:color(display-p3 1 1 1)){:root{';
+
+  // Object.keys(theme).map((themeVar) => {
+  //   pageConfig.styles += toCSSVariable(themeVar) + `:${theme[themeVar]};`;
+  //   // pageConfig.styles +=
+  //   //   toCSSVariable(themeVar) + `:${toDisplayP3(theme[themeVar])};`;
+  // });
+
+  pageConfig.styles += '}</style>';
+
+  return pageConfig;
+};
+
+export { toPageConfig as default };
