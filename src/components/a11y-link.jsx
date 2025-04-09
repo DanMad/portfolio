4be097cf20ec951c286a3c46e33e3e2bd6ede71c
DanMad <hooky.mounted_0n@icloud.com>
2025-04-09 21:36:10 +1000

    Updaed A11yLink

--- src/components/a11y-link.jsx
+++ src/components/a11y-link.jsx
@@ -1,10 +1,18 @@
+import Button from 'components/button';
 import 'styles/a11y-link';
 
 function A11yLink() {
+  const handleClick = () => {
+    // window.location.hash is used here to ensure default browser behavior,
+    // instead of useNavigate from react-router.
+    window.location.hash = '';
+    window.location.hash = '#main';
+  };
+
   return (
-    <a href="#main" className="a11y-link">
-      Skip to main content
-    </a>
+    <Button className="a11y-link" onClick={handleClick} type="secondary">
+      Skip to Content
+    </Button>
   );
 }
 
